7817f2caff6443c0dc395d0d45b220494604b873
XEdP3X <xedp3x@online.de>
2012-12-16 13:36:25 +0100

    Alle Berliner Bezirke

--- config/Berlin.php
+++ config/Berlin.php
@@ -76,4 +76,72 @@ $_templates[] = array(
         "down"  => "http://www.berlin.de/ba-friedrichshain-kreuzberg/bvv-online/"
 );
 
+$_templates[] = array(
+		"id"    => "BE_CW_ALLRIS",              // Eindeutige ID vom Template
+		"type"  => "PL_ALLRIS",                 // Plugin Name
+		"name"  => "BE Cha-Wil Allris",    	// Anzeigenanme
 
+		// Plugin Settings
+		"url"   => "http://www.berlin.de/ba-charlottenburg-wilmersdorf/bvv-online/vo020.asp?VOLFDNR=",
+		"down"  => "http://www.berlin.de/ba-charlottenburg-wilmersdorf/bvv-online/"
+);
+
+$_templates[] = array(
+		"id"    => "BE_LB_ALLRIS",              // Eindeutige ID vom Template
+		"type"  => "PL_ALLRIS",                 // Plugin Name
+		"name"  => "BE Lichtenberg Allris",    	// Anzeigenanme
+
+		// Plugin Settings
+		"url"   => "http://www.berlin.de/ba-lichtenberg/bvv-online/vo020.asp?VOLFDNR=",
+		"down"  => "http://www.berlin.de/ba-lichtenberg/bvv-online/"
+);
+
+$_templates[] = array(
+		"id"    => "BE_MI_ALLRIS",              // Eindeutige ID vom Template
+		"type"  => "PL_ALLRIS",                 // Plugin Name
+		"name"  => "BE Mitte Allris",    	// Anzeigenanme
+
+		// Plugin Settings
+		"url"   => "http://www.berlin.de/ba-mitte/bvv-online/vo020.asp?vo020.asp?VOLFDNR=",
+		"down"  => "http://www.berlin.de/ba-mitte/bvv-online/vo020.asp?"
+);
+
+$_templates[] = array(
+		"id"    => "BE_PA_ALLRIS",              // Eindeutige ID vom Template
+		"type"  => "PL_ALLRIS",                 // Plugin Name
+		"name"  => "BE Pankow Allris",    	// Anzeigenanme
+
+		// Plugin Settings
+		"url"   => "http://www.berlin.de/ba-pankow/bvv-online/vo020.asp?VOLFDNR=",
+		"down"  => "http://www.berlin.de/ba-pankow/bvv-online/"
+);
+
+$_templates[] = array(
+		"id"    => "BE_RE_ALLRIS",              // Eindeutige ID vom Template
+		"type"  => "PL_ALLRIS",                 // Plugin Name
+		"name"  => "BE Reinickendorf Allris",    	// Anzeigenanme
+
+		// Plugin Settings
+		"url"   => "http://www.berlin.de/ba-reinickendorf/bvv-online/vo020.asp?VOLFDNR=",
+		"down"  => "http://www.berlin.de/ba-reinickendorf/bvv-online/"
+);
+
+$_templates[] = array(
+		"id"    => "BE_SP_ALLRIS",              // Eindeutige ID vom Template
+		"type"  => "PL_ALLRIS",                 // Plugin Name
+		"name"  => "BE Spandau Allris",    	// Anzeigenanme
+
+		// Plugin Settings
+		"url"   => "http://www.berlin.de/ba-spandau/bvv-online/vo020.asp?VOLFDNR=",
+		"down"  => "http://www.berlin.de/ba-spandau/bvv-online/"
+);
+
+$_templates[] = array(
+		"id"    => "BE_TS_ALLRIS",              // Eindeutige ID vom Template
+		"type"  => "PL_ALLRIS",                 // Plugin Name
+		"name"  => "BE Tempel-Sh Allris",    	// Anzeigenanme
+
+		// Plugin Settings
+		"url"   => "http://www.berlin.de/ba-tempelhof-schoeneberg/bvv-online/vo020.asp?VOLFDNR=",
+		"down"  => "http://www.berlin.de/ba-tempelhof-schoeneberg/bvv-online/"
+);
